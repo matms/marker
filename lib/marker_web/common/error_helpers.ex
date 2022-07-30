ec342a672fdd3235933805d3b17c4bbc492f60b3
matms <matm31415@gmail.com>
2022-07-30 19:01:20 -0400

    Styled all account views.

--- lib/marker_web/common/error_helpers.ex
+++ lib/marker_web/common/error_helpers.ex
@@ -8,12 +8,12 @@ defmodule MarkerWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field) do
+  def error_tag(form, field, opts \\ []) do
+    opts =
+      Keyword.merge([class: "form-error-tag", phx_feedback_for: input_name(form, field)], opts)
+
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
-        phx_feedback_for: input_name(form, field)
-      )
+      content_tag(:span, translate_error(error), opts)
     end)
   end
 
